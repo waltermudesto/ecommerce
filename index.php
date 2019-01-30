28948e78914865960e5546be6cc18d51c52f402a
Walter Mudesto <walter.mudesto1971@gmail.com>
2019-01-30 11:05:15 -0200

    Criando a classe PageAdmin

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
+
 
 $app = new Slim();
 
@@ -16,6 +18,15 @@ $app->get('/', function() {
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
 
 $app->run();
 
