f5ee7478031aead8ab444d8e6eb8a497adf7ac28
Walter Mudesto <walter.mudesto1971@gmail.com>
2019-02-05 14:09:56 -0200

    Carrinho Calculo de Frete

--- site.php
+++ site.php
@@ -68,9 +68,11 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart",[
+	
+	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getproducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 
 });
@@ -120,6 +122,17 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart =  Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /cart");
 	exit;
