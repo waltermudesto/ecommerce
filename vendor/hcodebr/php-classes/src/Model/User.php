b9b0407f63ca7deee05382131029b1b360b45b4f
Walter Mudesto <walter.mudesto1971@gmail.com>
2019-02-06 10:13:34 -0200

    Site Cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,7 @@ class User extends Model {
 	const SESSION = "User";
 	const SECRET = "HcodePhp7_Secret";
 	const ERROR = "UserError";
+	const ERROR_REGISTER = "UserErrorRegister";
 
 	public static function getFromSession()
 	{
@@ -342,6 +343,37 @@ class User extends Model {
 			]);
 		}
 
+		public static function setErrorRegister($msg)
+		{
+
+			$_SESSION[User::ERROR_REGISTER] = $msg;
+		}
+
+		public static function getErrorRegister()
+		{
+
+			$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+			User::clearErrorRegister();
+			return $msg;
+		}
+
+		public static function clearErrorRegister()
+		{
+
+			$_SESSION[User::ERROR_REGISTER] =  NULL;
+		}
+
+		public static function checkLoginExist($login)
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+				':deslogin'=>$login
+			]);
+
+			return (count($results) > 0);
+		}
 }
 
 
