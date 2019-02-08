eb0db25dd111e26a14758cf467151757355ee617
Walter Mudesto <walter.mudesto1971@gmail.com>
2019-02-08 14:26:53 -0200

    Admin Pedidos

--- index.php
+++ index.php
@@ -11,12 +11,13 @@ $app = new Slim();
 
 $app->config("debug", true);
 
+require_once("functions.php");
 require_once("site.php");
 require_once("admin.php");
 require_once("admin-users.php");
 require_once("admin-categories.php");
 require_once("admin-products.php");
-require_once("functions.php");
+require_once("admin-orders.php");
 
 $app->run();
 
